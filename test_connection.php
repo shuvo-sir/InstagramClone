66d80d9e8cce8376fd1a9fe246610a9ba2dff444
Shuvo Halder <shuvohalder499@gmail.com>
2024-12-14 19:36:59 +0600

    This is a basic Instagram code

---
+++ test_connection.php
@@ -0,0 +1,13 @@
+<?php
+$servername = 'localhost';
+$user       = 'root';
+$pass       = '';
+$db         = 'insta_db';
+
+$conn = mysqli_connect($servername, $user, $pass, $db);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
